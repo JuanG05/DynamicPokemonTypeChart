97802127f9a455978c83d5a1d6a7b6b0266cf6a4
Juan Garro <juangarro05@gmail.com>
2023-03-11 14:46:20 -0600

    Code from previous work, search pokemon popover

--- PokeTypeChart/src/app/components/pokemon-search-popover/pokemon-search-popover.component.ts
+++ PokeTypeChart/src/app/components/pokemon-search-popover/pokemon-search-popover.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Output, Input, EventEmitter} from '@angular/core';
+import {Pokemon} from 'src/app/models/Pokemon';
 
 @Component({
   selector: 'app-pokemon-search-popover',
   templateUrl: './pokemon-search-popover.component.html',
-  styleUrls: ['./pokemon-search-popover.component.scss'],
+  styleUrls: ['./pokemon-search-popover.component.scss']
 })
-export class PokemonSearchPopoverComponent  implements OnInit {
+export class PokemonSearchPopoverComponent implements OnInit {
+  @Input() filteredSearch: Pokemon[] | undefined;
+  @Output() selectedPokemon = new EventEmitter<Pokemon>();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
-
+  outputSelectedPokemon(pokemon: Pokemon) {
+    this.selectedPokemon.emit(pokemon);
+  }
 }
