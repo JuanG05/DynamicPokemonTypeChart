0f0ec16adde5f92fbbf44703fee60e6ae6a49691
Juan Garro <juangarro05@gmail.com>
2023-03-11 18:23:02 -0600

    Add About component

---
+++ PokeTypeChart/src/app/components/about/about.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+import {ModalController} from '@ionic/angular';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+  constructor(private modalController: ModalController) {}
+
+  ngOnInit() {}
+
+  closeModal() {
+    this.modalController.dismiss();
+  }
+}
